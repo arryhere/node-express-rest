a926ea3367d2e6a1ac2fba4f3df6d2cb1b907d2f
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-08-27 15:21:25 +0530

    feat: added logger

--- src/helpers/log.helper.ts
+++ src/helpers/log.helper.ts
@@ -3,10 +3,10 @@ import pretty from 'pino-pretty';
 
 const logger = pino(pretty());
 
-export function log_info(message: string, additional_data = {}) {
-  logger.info(additional_data, message);
+export function log_info(message: string) {
+  logger.info(message);
 }
 
-export function log_error(message: string, additional_data = {}) {
-  logger.error(additional_data, message);
+export function log_error(message: string) {
+  logger.error(message);
 }
