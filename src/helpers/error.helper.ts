a73fcedba94df02e1fa73d024b0dfa7cc98de9c6
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-10-01 18:02:00 +0530

    feat: context binding, files arranged

--- src/helpers/error.helper.ts
+++ src/helpers/error.helper.ts
@@ -1,12 +1,9 @@
 export default class Exception extends Error {
-  status_code: number;
-  message: string;
-  data: object;
-
-  constructor(message: string, status_code: number, data: object) {
+  constructor(
+    message: string,
+    private readonly status_code: number,
+    private readonly data: object
+  ) {
     super(message);
-    this.status_code = status_code;
-    this.message = message;
-    this.data = data;
   }
 }
