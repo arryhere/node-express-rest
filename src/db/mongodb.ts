45b016ef38139d7640ba9312550badc376a30d8e
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-10-04 15:06:40 +0530

    feat: db connect type fixed

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -3,7 +3,7 @@ import { log_info } from '../common/helper/log.helper.js';
 import { config } from '../config/config.js';
 
 export async function mongodb() {
-  const uri = config.db.uri as string;
+  const uri = config.db.uri;
   await connect(uri);
 
   log_info('mongodb connection success');
